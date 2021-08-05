27fdd7bc84c42814275cd62014ebe0b2b0932e7f
Sneakometer <sneakometer@gmail.com>
2021-08-05 06:29:35 +0200

    :bug: Implement own equals and hashcode for MinecraftIdentity
    - Lombok returned false for identical instances for some reason

--- src/main/java/eu/smashmc/api/identity/minecraft/MinecraftIdentity.java
+++ src/main/java/eu/smashmc/api/identity/minecraft/MinecraftIdentity.java
@@ -4,22 +4,18 @@ import java.util.UUID;
 
 import eu.smashmc.api.identity.Identity;
 import eu.smashmc.api.identity.minecraft.property.TexturesProperty;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@EqualsAndHashCode
 @ToString
 public class MinecraftIdentity implements Identity<UUID> {
 
 	private final UUID uuid;
 
-	@EqualsAndHashCode.Exclude
 	private final String name;
 
 	@ToString.Exclude
-	@EqualsAndHashCode.Exclude
 	private final TexturesProperty texture;
 
 	public MinecraftIdentity(UUID uuid, String name) {
@@ -46,5 +42,30 @@ public class MinecraftIdentity implements Identity<UUID> {
 			throw new IllegalStateException("texture not loaded");
 		}
 		return texture;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MinecraftIdentity other = (MinecraftIdentity) obj;
+		if (uuid == null) {
+			if (other.uuid != null)
+				return false;
+		} else if (!uuid.equals(other.uuid))
+			return false;
+		return true;
 	}
 }
