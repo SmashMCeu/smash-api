855a2785135426564ed64f6441c0ad5d8e841c74
Sneakometer <sneakometer@gmail.com>
2021-08-08 02:32:52 +0200

    :sparkles: Add Constants

---
+++ src/main/java/eu/smashmc/api/Constants.java
@@ -0,0 +1,21 @@
+package eu.smashmc.api;
+
+public class Constants {
+
+	/**
+	 * Global chat prefix for serverwide messages.
+	 */
+	public static final String PREFIX = "§8● §aSmashMC §8➥ §7";
+
+	/**
+	 * Chat Messages are constructed in the following pattern: <br>
+	 * format(CHAT_FORMAT, coloredGroupName, coloredPlayerName, message)
+	 */
+	public static final String CHAT_FORMAT = "%s §8● %s §8➥ §f%s";
+
+	/**
+	 * Tab prefix is constructed in the following way: <br>
+	 * format(TAB_PREFIX_FORMAT, coloredGroupTabName)
+	 */
+	public static final String TAB_PREFIX_FORMAT = "%s §8┃ ";
+}
