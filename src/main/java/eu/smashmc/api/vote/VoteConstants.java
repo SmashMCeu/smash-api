4100919ab43d741010c0255538798152a727bd66
Sneakometer <50603525+Sneakometer@users.noreply.github.com>
2021-10-16 00:14:26 +0200

    Revert "Updating vote-api"

--- src/main/java/eu/smashmc/api/vote/VoteConstants.java
+++
@@ -1,7 +0,0 @@
-package eu.smashmc.api.vote;
-
-public class VoteConstants {
-
-    public static final String CONTAINER_RMAP = "votecontainers";
-    public static final String STREAK_RMAP = "votestreaks";
-}
--- src/main/java/eu/smashmc/api/vote/VoteContainer.java
+++ src/main/java/eu/smashmc/api/vote/VoteContainer.java
@@ -1,47 +1,47 @@
 package eu.smashmc.api.vote;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
 @RequiredArgsConstructor
 public class VoteContainer {
 
-    private final List<VoteEntry> voteEntries = new ArrayList<>();
-    /**
-     * Get the uuid of the player who sends all votes
-     *
-     * @return players' uuid
-     */
-    @Getter private final UUID uuid;
+	private final List<VoteEntry> voteEntries = new ArrayList<>();
+	/**
+	 * Get the name of the player who sends all votes
+	 *
+	 */
+	@Getter
+	private final String playerName;
 
-    /**
-     * Clear all contained votes
-     */
-    public void clear() {
-        this.voteEntries.clear();
-    }
+	/**
+	 * Clear all contained votes
+	 */
+	public void clear() {
+		this.voteEntries.clear();
+	}
 
-    /**
-     * Add a vote to the container
-     */
-    public void addVote(@NonNull VoteEntry vote) {
-        this.voteEntries.add(vote);
-    }
+	/**
+	 * Add a vote to the container
+	 * 
+	 * @param vote the vote to be added
+	 */
+	public void addVote(@NonNull VoteEntry vote) {
+		this.voteEntries.add(vote);
+	}
 
-    /**
-     * Get all votes
-     *
-     * @return a immutable list of all votes
-     */
-    public List<VoteEntry> getVotes() {
-        return Collections.unmodifiableList(this.voteEntries);
-    }
-
+	/**
+	 * Get all votes
+	 *
+	 * @return a immutable list of all votes
+	 */
+	public List<VoteEntry> getVotes() {
+		return Collections.unmodifiableList(this.voteEntries);
+	}
 
 }
