e311eaf23f0ffb79d920f62a150481fec56d7b8e
Sneakometer <sneakometer@gmail.com>
2021-11-10 01:31:47 +0100

    :sparkles: Add economy API

---
+++ src/main/java/eu/smashmc/api/economy/Currency.java
@@ -0,0 +1,11 @@
+package eu.smashmc.api.economy;
+
+/**
+ * A currency.
+ * 
+ * @author LiquidDev
+ *
+ */
+public enum Currency {
+	COINS, GEMS;
+}
---
+++ src/main/java/eu/smashmc/api/economy/Transaction.java
@@ -0,0 +1,46 @@
+package eu.smashmc.api.economy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import eu.smashmc.api.SmashMc;
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * Class representing a database operation consisting of multiple
+ * {@link Trade}s.
+ * 
+ * @author LiquidDev
+ *
+ */
+public class Transaction {
+
+	@Getter
+	private List<Trade> trades;
+
+	public Transaction(@NonNull List<Trade> trades) {
+		this.trades = trades;
+	}
+
+	public Transaction() {
+		this(new ArrayList<>());
+	}
+
+	public CompletableFuture<Void> commit() {
+		return SmashMc.getComponent(Economy.class)
+				.submitTransaction(this);
+	}
+
+	public void addBalance(double amount, Currency currency, UUID accountHolder, String reason, String description) {
+		var trade = new Trade(accountHolder, currency, amount, reason, description);
+		this.trades.add(trade);
+	}
+
+	public void removeBalance(double amount, Currency currency, UUID accountHolder, String reason, String description) {
+		var add = -amount;
+		this.addBalance(add, currency, accountHolder, reason, description);
+	}
+}
