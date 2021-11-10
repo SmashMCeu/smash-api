12c4b0236ee9bed74dc7842474e7506f4a1bcec8
cokejoke <justin@knoter.eu>
2021-11-10 22:44:46 +0100

    :sparkles: Add fromId method in FriendAllowType

--- src/main/java/eu/smashmc/api/friends/FriendAllowType.java
+++ src/main/java/eu/smashmc/api/friends/FriendAllowType.java
@@ -1,5 +1,22 @@
 package eu.smashmc.api.friends;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum FriendAllowType {
-    ALL, ONLY_FRIENDS, NOBODY
+    ALL(1), ONLY_FRIENDS(2), NOBODY(3);
+
+    private final int id;
+
+    public static FriendAllowType fromId(int id) {
+        for(FriendAllowType friendAllowType : values()) {
+            if(friendAllowType.getId() == id) {
+                return friendAllowType;
+            }
+        }
+        return FriendAllowType.ALL;
+    }
+
 }
