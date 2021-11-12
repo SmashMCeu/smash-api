75b7f46b0b93f606bba58bafb9634cf6ea53d9bf
Sneakometer <sneakometer@gmail.com>
2021-11-12 05:51:39 +0100

    :sparkles: Add PlayerBalanceChangeEvent

---
+++ src/main/java/eu/smashmc/api/economy/PlayerBalanceChangeEvent.java
@@ -0,0 +1,41 @@
+package eu.smashmc.api.economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+import lombok.Getter;
+import net.md_5.bungee.api.ServerPing.Players;
+
+/**
+ * {@link Bukkit} event fired when a {@link Players} balance of any currency
+ * type changes for any reason.
+ * 
+ * @author LiquidDev
+ *
+ */
+@Getter
+public class PlayerBalanceChangeEvent extends PlayerEvent {
+
+	private Currency currency;
+	private double balance;
+
+	public PlayerBalanceChangeEvent(@NotNull Player who, Currency currency, double balance) {
+		super(who);
+		this.currency = currency;
+		this.balance = balance;
+	}
+
+	private static final HandlerList handlers = new HandlerList();
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
