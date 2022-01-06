27e6cb2de6f623affebb089b47764ffae6eb5412
Sneakometer <sneakometer@gmail.com>
2022-01-06 10:35:23 +0100

    :memo: Add missing line breaks to AutoRegister javadoc

--- src/main/java/eu/smashmc/api/core/AutoRegister.java
+++ src/main/java/eu/smashmc/api/core/AutoRegister.java
@@ -10,27 +10,29 @@ import org.bukkit.event.Listener;
 import eu.smashmc.api.SmashComponent;
 
 /**
- * TODO Experimental
+ * TODO Experimental<br>
+ * <br>
  * 
  * Allows you to automatically instantiate and register listeners, commands and
  * more. Also allows you to use {@link AutoInject} to automatically inject
- * dependencies into fields and constructors.
- * 
+ * dependencies into fields and constructors. <br>
+ * <br>
  * List of supported registrations: <br>
  * - Bukkit {@link Listener} <br>
  * - Commands using liquidcommands library <br>
  * - {@link SmashComponent} implementations <br>
- * - TODO: soon more
+ * - TODO: soon more <br>
+ * <br>
  * 
  * Instances of all instantiated classes can be retrieved using the
- * {@link Registered} helper class.
+ * {@link Registered} helper class. <br>
+ * <br>
  *
- * **Troubleshooting**
- * 
  * Reasons for why a class might not automatically be registered: <br>
  * - The Plugin does not depend on SmashCore<br>
  * - The class is not in a package starting with "eu.smashmc"<br>
  * - The class or it's package name contain the word 'bungee' (case insensitive)
+ * <br>
  * 
  * @author LiquidDev
  *
