b6324006296431360c14300faaea92bf954d1fbf
Sneakometer <sneakometer@gmail.com>
2022-02-26 19:25:34 +0100

    :sparkles: Add CommonTranslations

---
+++ src/main/java/eu/smashmc/api/lang/CommonTranslations.java
@@ -0,0 +1,23 @@
+package eu.smashmc.api.lang;
+
+/**
+ * List of common translation keys for easy access.
+ * 
+ */
+public class CommonTranslations {
+
+	public static String CHAT_COOLDOWN = "chat.cooldown";
+	public static String CHAT_NO_PERMISSION = "chat.no_permission";
+	public static String CHAT_INVALID_ARGUMENTS = "chat.invalid_arguments";
+	public static String CHAT_INVALID_ARGUMENTS_USE = "chat.invalid_arguments_use";
+	public static String CHAT_PLAYER_OFFLINE = "chat.player_offline";
+	public static String CHAT_PLAYER_UNKNOWN = "chat.player_unknown";
+	public static String CHAT_ONLY_FOR_PLAYERS = "chat.command.players_only";
+	public static String CHAT_TOO_EXPENSIVE = "chat.too_expensive";
+	public static String CHAT_PLAYER_JOIN = "chat.player_join";
+	public static String CHAT_PLAYER_LEAVE = "chat.player_leave";
+	public static String CHAT_BUTTON_ACCEPT = "chat.button.accept";
+	public static String CHAT_BUTTON_DENY = "hat.button.deny";
+	public static String CHAT_BUTTON_CONFIRM = "chat.button.confirm";
+
+}
