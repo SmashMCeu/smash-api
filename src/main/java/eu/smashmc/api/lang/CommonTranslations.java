b9c1fc8e842effee7badeddb0791bc9d52708869
Packsolite <packsolite@gmail.com>
2022-05-10 22:57:32 +0200

    :sparkles: Add CHAT_ONLY_FOR_PRO CommonTranslations

--- src/main/java/eu/smashmc/api/lang/CommonTranslations.java
+++ src/main/java/eu/smashmc/api/lang/CommonTranslations.java
@@ -6,19 +6,20 @@ package eu.smashmc.api.lang;
  */
 public class CommonTranslations {
 
-	public static String CHAT_COOLDOWN = "chat.cooldown";
-	public static String CHAT_ERROR = "chat.error";
-	public static String CHAT_NO_PERMISSION = "chat.no_permission";
-	public static String CHAT_INVALID_ARGUMENTS = "chat.invalid_arguments";
-	public static String CHAT_INVALID_ARGUMENTS_USE = "chat.invalid_arguments_use";
-	public static String CHAT_PLAYER_OFFLINE = "chat.player_offline";
-	public static String CHAT_PLAYER_UNKNOWN = "chat.player_unknown";
-	public static String CHAT_ONLY_FOR_PLAYERS = "chat.command.players_only";
-	public static String CHAT_TOO_EXPENSIVE = "chat.too_expensive";
-	public static String CHAT_PLAYER_JOIN = "chat.player_join";
-	public static String CHAT_PLAYER_LEAVE = "chat.player_leave";
-	public static String CHAT_BUTTON_ACCEPT = "chat.button.accept";
-	public static String CHAT_BUTTON_DENY = "hat.button.deny";
-	public static String CHAT_BUTTON_CONFIRM = "chat.button.confirm";
+	public static final String CHAT_COOLDOWN = "chat.cooldown";
+	public static final String CHAT_ERROR = "chat.error";
+	public static final String CHAT_NO_PERMISSION = "chat.no_permission";
+	public static final String CHAT_INVALID_ARGUMENTS = "chat.invalid_arguments";
+	public static final String CHAT_INVALID_ARGUMENTS_USE = "chat.invalid_arguments_use";
+	public static final String CHAT_PLAYER_OFFLINE = "chat.player_offline";
+	public static final String CHAT_PLAYER_UNKNOWN = "chat.player_unknown";
+	public static final String CHAT_ONLY_FOR_PLAYERS = "chat.command.players_only";
+	public static final String CHAT_PRO_ONLY_FEATURE = "chat.pro";
+	public static final String CHAT_TOO_EXPENSIVE = "chat.too_expensive";
+	public static final String CHAT_PLAYER_JOIN = "chat.player_join";
+	public static final String CHAT_PLAYER_LEAVE = "chat.player_leave";
+	public static final String CHAT_BUTTON_ACCEPT = "chat.button.accept";
+	public static final String CHAT_BUTTON_DENY = "hat.button.deny";
+	public static final String CHAT_BUTTON_CONFIRM = "chat.button.confirm";
 
 }
