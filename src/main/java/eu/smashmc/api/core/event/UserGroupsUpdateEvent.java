5d8cd4ea9277596456aa7ea6cd1dbeb4d87673b1
Packsolite <packsolite@gmail.com>
2022-05-17 23:22:34 +0200

    :sparkles: Add UserGroupsUpdateEvent

---
+++ src/main/java/eu/smashmc/api/core/event/UserGroupsUpdateEvent.java
@@ -0,0 +1,37 @@
+package eu.smashmc.api.core.event;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import lombok.Getter;
+
+/**
+ * Called when the groups of a player were updated.
+ */
+@Getter
+public class UserGroupsUpdateEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+
+	private final UUID uuid;
+	private final List<String> updatedGroups;
+
+	public UserGroupsUpdateEvent(@NotNull UUID uuid, @NotNull List<String> groups) {
+		this.uuid = uuid;
+		this.updatedGroups = Collections.unmodifiableList(groups);
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
