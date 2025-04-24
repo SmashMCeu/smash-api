35e9a9e0883752a201f7e707dac6ee4b2b4291be
Packsolite <packsolite@gmail.com>
2025-04-24 17:22:49 +0200

    :sparkles: Add vanish mode spectator

--- src/main/java/eu/smashmc/api/vanish/Vanish.java
+++ src/main/java/eu/smashmc/api/vanish/Vanish.java
@@ -1,60 +1,63 @@
 package eu.smashmc.api.vanish;
 
+import eu.smashmc.api.Environment;
+import eu.smashmc.api.SmashComponent;
+import lombok.Getter;
+
 import java.util.UUID;
 import java.util.function.BiPredicate;
 
-import eu.smashmc.api.Environment;
-import eu.smashmc.api.SmashComponent;
-
 /**
  * API for the vanish Plugin.
- * 
+ *
  * @param <T> type of player
  */
-@SmashComponent({ Environment.BUKKIT, Environment.BUNGEECORD })
+@SmashComponent({Environment.BUKKIT, Environment.BUNGEECORD})
 public interface Vanish<T> {
 
-	static final String defaultMessageChannel = "smashlib:vanish";
-	static final String commandPermission = "proxy.vanish";
+	@Getter
+	String defaultMessageChannel = "smashlib:vanish";
+	@Getter
+	String commandPermission = "proxy.vanish";
 
-	public static String getDefaultMessageChannel() {
-		return defaultMessageChannel;
-	}
-
-	public static String getCommandPermission() {
-		return commandPermission;
-	}
-
-	public default boolean isVanished(UUID uuid) {
+	default boolean isVanished(UUID uuid) {
 		return getVanishMode(uuid) == VanishMode.INVISIBLE;
 	}
 
-	public default boolean isVanished(T player) {
+	default boolean isVanished(T player) {
 		return getVanishMode(player) == VanishMode.INVISIBLE;
 	}
 
-	public default boolean isUndercover(UUID uuid) {
+	default boolean isUndercover(UUID uuid) {
 		return getVanishMode(uuid) == VanishMode.UNDERCOVER;
 	}
 
-	public default boolean isUndercover(T player) {
+	default boolean isUndercover(T player) {
 		return getVanishMode(player) == VanishMode.UNDERCOVER;
 	}
 
-	public VanishMode getVanishMode(T player);
+	default boolean isJustSpectating(UUID uuid) {
+		return getVanishMode(uuid) == VanishMode.SPECTATE;
+	}
 
-	public VanishMode getVanishMode(UUID uuid);
+	default boolean isJustSpectating(T player) {
+		return getVanishMode(player) == VanishMode.SPECTATE;
+	}
 
-	public int getVanishedPlayerCount();
+	VanishMode getVanishMode(T player);
 
-	public void registerListener(VanishListener<T> listener);
+	VanishMode getVanishMode(UUID uuid);
+
+	int getVanishedPlayerCount();
+
+	void registerListener(VanishListener<T> listener);
 
 	/**
 	 * Can be used so unvanishing does not cause problems with spectators being
 	 * visible. The function should return false if player 1 cannot see player 2.
-	 * 
+	 *
 	 * @param function function that returns true if the player can see the other
 	 *                 one
 	 */
-	public void setCanSeeFunction(BiPredicate<T, T> function);
+	void setCanSeeFunction(BiPredicate<T, T> function);
 }
--- src/main/java/eu/smashmc/api/vanish/VanishMode.java
+++ src/main/java/eu/smashmc/api/vanish/VanishMode.java
@@ -1,5 +1,13 @@
 package eu.smashmc.api.vanish;
 
 public enum VanishMode {
-	NONE, UNDERCOVER, INVISIBLE;
+	NONE, UNDERCOVER, INVISIBLE, SPECTATE;
+
+	public boolean isInvisibleToOthers() {
+		return this == INVISIBLE;
+	}
+
+	public boolean isSpectator() {
+		return this == UNDERCOVER || this == SPECTATE;
+	}
 }
