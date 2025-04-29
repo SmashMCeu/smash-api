611a0472267d9afc98f3e4e338ca0f56362a7f80
Packsolite <packsolite@gmail.com>
2025-04-30 01:03:28 +0200

    :sparkles: Add chat.proplus key

--- src/main/java/eu/smashmc/api/lang/CommonTranslations.java
+++ src/main/java/eu/smashmc/api/lang/CommonTranslations.java
@@ -7,6 +7,7 @@ public class CommonTranslations {
 
 	/* General */
 	public static final String CHAT_PRO_ONLY_FEATURE = "chat.pro";
+	public static final String CHAT_PROPLUS_ONLY_FEATURE = "chat.proplus";
 	public static final String CHAT_TOO_EXPENSIVE = "chat.too_expensive";
 	public static final String CHAT_PLEASE_REJOIN = "chat.rejoin_to_apply";
 
