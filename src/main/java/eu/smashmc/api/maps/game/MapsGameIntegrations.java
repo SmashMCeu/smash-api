137fdcb32436fa0be839584e75ba7b2a42f6b906
Packsolite <packsolite@gmail.com>
2025-05-24 04:19:03 +0200

    :bug: Refresh map meta before loading

--- src/main/java/eu/smashmc/api/maps/game/MapsGameIntegrations.java
+++ src/main/java/eu/smashmc/api/maps/game/MapsGameIntegrations.java
@@ -2,9 +2,11 @@ package eu.smashmc.api.maps.game;
 
 import de.liquiddev.command.autocomplete.Autocompleter;
 import eu.smashmc.api.maps.game.voting.MapVotingIntegration;
+import eu.smashmc.api.maps.map.MinecraftMap;
 import eu.smashmc.api.maps.map.MinecraftMapOverview;
 import org.bukkit.entity.Player;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 public interface MapsGameIntegrations {
@@ -20,4 +22,8 @@ public interface MapsGameIntegrations {
 	 */
 	void playMap(MinecraftMapOverview map);
 
+	/**
+	 * Forces a metadata refresh for the given map.
+	 */
+	CompletableFuture<MinecraftMap> refreshMap(MinecraftMap map);
 }
