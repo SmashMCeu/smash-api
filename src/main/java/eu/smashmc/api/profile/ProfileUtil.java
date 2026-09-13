d5e0f9ee96b9a468c8cc270d1672d5b6b1a18c66
Packsolite <packsolite@gmail.com>
2026-09-13 02:48:59 +0200

    :zap: Cache PlayerProfileService in ProfileUtil

--- src/main/java/eu/smashmc/api/profile/ProfileUtil.java
+++ src/main/java/eu/smashmc/api/profile/ProfileUtil.java
@@ -1,41 +1,43 @@
 package eu.smashmc.api.profile;
 
+import com.google.common.base.Suppliers;
 import eu.smashmc.api.PlayerOfflineException;
+import eu.smashmc.api.SmashMc;
 import org.bukkit.entity.Player;
 
-import eu.smashmc.api.SmashMc;
+import java.util.function.Supplier;
 
 /**
  * A simple {@link PlayerProfileService} wrapper for easy profile access.
- * 
+ *
  * @author LiquidDev
  *
  */
 public class ProfileUtil {
 
+	private static final Supplier<PlayerProfileService> PROFILE_SERVICE_SUPPLIER = Suppliers.memoize(() -> SmashMc.getComponent(PlayerProfileService.class));
+
 	/**
 	 * Get a players profile.
-	 * 
-	 * @see PlayerProfileService#getProfile(java.util.UUID)
+	 *
 	 * @param player the player to get the profile of
 	 * @return the players {@link PlayerProfile}
+	 * @see PlayerProfileService#getProfile(java.util.UUID)
 	 */
 	public static PlayerProfile getProfile(Player player) {
-		if(!player.isOnline()){
+		if (!player.isOnline()) {
 			throw new PlayerOfflineException(player);
 		}
-		return SmashMc.getComponent(PlayerProfileService.class)
-				.getProfile(player.getUniqueId());
+		return PROFILE_SERVICE_SUPPLIER.get().getProfile(player.getUniqueId());
 	}
 
 	/**
 	 * Saves specific topics of a profile.
-	 * 
+	 *
 	 * @param profile the {@link PlayerProfile} to update
 	 * @param topics  the topics to be saved.
 	 */
 	public static void saveProfile(PlayerProfile profile, String... topics) {
-		SmashMc.getComponent(PlayerProfileService.class)
-				.updateProfile(profile, topics);
+		PROFILE_SERVICE_SUPPLIER.get().updateProfile(profile, topics);
 	}
 }
